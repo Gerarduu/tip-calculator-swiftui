f9909c8d72b22df9684a61b26d4cdebb81a0e646
Gerarduu <gerar.riera@gmail.com>
2023-11-12 10:20:02 +0100

    Initial Commit

---
+++ MyProject/MyProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyProjectApp.swift
+//  MyProject
+//
+//  Created by Gerard Riera on 12/11/23.
+//
+
+import SwiftUI
+
+@main
+struct MyProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
